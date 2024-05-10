43aee5567c0f62d1757a007ee356f8e805e604ae
Костянтин Гольцев <ipz224_gko@student.ztu.edu.ua>
2024-05-11 00:23:40 +0300

    Lab05 state

---
+++ Lab05/ClassLibrary/State/CodeViewModeState.cs
@@ -0,0 +1,13 @@
+﻿
+using ClassLibrary.Iterator;
+
+namespace ClassLibrary.State
+{
+    public class CodeViewModeState : IViewModeState
+    {
+        public void Render(HTMLDocument document)
+        {
+            Console.WriteLine("Rendering HTML document as code:");
+        }
+    }
+}
