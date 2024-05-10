73e72bb87bb9744b3503c83fbbc895b39116ed41
Костянтин Гольцев <ipz224_gko@student.ztu.edu.ua>
2024-05-10 20:37:50 +0300

    Lab05

---
+++ Lab05/ClassLibrary/LightHTML/LightNode.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ClassLibrary.Composite
+{
+    public abstract class LightNode
+    {
+        public abstract string OuterHTML { get; }
+        public abstract string InnerHTML { get; }
+        public abstract string ToHTML();
+    }
+}
