165362331ed131c5d993a428fd47ba5478991c02
Костянтин Гольцев <ipz224_gko@student.ztu.edu.ua>
2024-05-15 14:44:08 +0300

    Deck class is not using singleton anymore. State base class added

---
+++ Lab06/ClassLibrary/CardDeck/Deck.cs
@@ -0,0 +1,42 @@
+﻿
+using ClassLibrary.Cards;
+using ClassLibrary.CardsFactory;
+using ClassLibrary.Extensions;
+
+namespace ClassLibrary.CardDeck
+{
+    public class Deck
+    {
+        private List<Card> cards;
+
+        public Deck()
+        {
+            cards = new List<Card>();
+            InitializeDeck();
+        }    
+
+        private void InitializeDeck()
+        {
+            foreach (Suit suit in Enum.GetValues(typeof(Suit)))
+            {
+                foreach (Rank rank in Enum.GetValues(typeof(Rank)))
+                {
+                    cards.Add(CardFactory.CreateCard(rank, suit));
+                }
+            }
+            cards.Shuffle();
+        }      
+
+        public Card DrawCard()
+        {
+            if (cards.Count == 0)
+            {
+                throw new InvalidOperationException("No cards left in the deck.");
+            }
+
+            Card card = cards[0];
+            cards.RemoveAt(0);
+            return card;       
+        }
+    }
+}
