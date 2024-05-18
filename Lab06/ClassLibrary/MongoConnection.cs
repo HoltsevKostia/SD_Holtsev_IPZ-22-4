0da52bbb838ad6ada9dead31b49f55b9a559a7a6
Костянтин Гольцев <ipz224_gko@student.ztu.edu.ua>
2024-05-18 16:55:41 +0300

    Added class to connect mongo db. Added class Player and PlayerManager to operate data concerning to player

---
+++ Lab06/ClassLibrary/MongoConnection.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Driver;
+
+namespace ClassLibrary
+{
+
+    public class MongoConnection
+    {
+        private static readonly object _lock = new object();
+        private static MongoConnection _instance;
+        private IMongoDatabase _database;
+
+        private MongoConnection()
+        {
+            var client = new MongoClient("your_mongo_connection_string");
+            _database = client.GetDatabase("your_database_name");
+        }
+
+        public static MongoConnection Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new MongoConnection();
+                    }
+                    return _instance;
+                }
+            }
+        }
+
+        public IMongoCollection<T> GetCollection<T>(string name)
+        {
+            return _database.GetCollection<T>(name);
+        }
+    }
+}
